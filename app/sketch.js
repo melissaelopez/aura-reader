e48ece8f5891506932a2c3635479b236013f6c43
Melissa Lopez <mel501@nyu.edu>
2017-12-07 13:11:29 -0500

    one color tracker

--- app/sketch.js
+++ app/sketch.js
@@ -4,19 +4,14 @@ var showCapture = true;
 var screenWidth = 360;
 var screenHeight = 270;
 
+var totalParticles = 300;
+
 var pixelsPerFrame1;
 var oldX1 = 0;
 var oldY1 = 0;
 
 var sumPPF1 = 0;
 var averagePPF1;
-
-var pixelsPerFrame2;
-var oldX2 = 0;
-var oldY2 = 0;
-
-var sumPPF2 = 0;
-var averagePPF2;
 
 // video capture object
 var capture;
@@ -34,9 +29,6 @@ var xPos1;
 var yPos1;
 
 var frameCheck = 15;
-
-// keep track of which color we are currently going to set (the user will click to
-// set color #1 and then click again to set color #2)
 var currentColor = 1;
 
 // what is our current threshold?  This is how sensitve our color detection algorithm should be
@@ -60,38 +52,33 @@ function setup() {
       // container.style('height', '100%');
 
       // start up our web cam
-  capture = createCapture({
-    video: {
-      mandatory: {
-        minWidth: screenWidth,
-        minHeight: screenHeight,
-        maxWidth: screenWidth,
-        maxHeight: screenHeight
-      }
+    capture = createCapture({
+        video: {
+            mandatory: {
+                minWidth: screenWidth,
+                minHeight: screenHeight,
+                maxWidth: screenWidth,
+                maxHeight: screenHeight
+          }
+        }
+    });
+    capture.hide();
+
+    stroke(0, 255, 0);
+    noFill();
+    rectMode(CENTER);
+
+    // request a detailed noise landscape
+    noiseDetail(24);
+
+    // create our walker array
+    walkerArray = [];
+
+    // create particle system
+    for (var i = 0; i < totalParticles; i++) {
+        var tempWalker = new NoiseWalker( random(screenWidth), random(screenHeight) );
+        walkerArray.push( tempWalker );
     }
-  });
-  capture.hide();
-
-  stroke(0, 255, 0);
-  noFill();
-  rectMode(CENTER);
-
-  // request a detailed noise landscape
-  noiseDetail(24);
-
-  // create our walker array
-  walkerArray = [];
-
-  // fill the walker array with 500 walkers!
-
-  for (var i = 0; i < 300; i++) {
-
-    // create a NoiseWalker
-    var tempWalker = new NoiseWalker( random(screenWidth), random(screenHeight) );
-
-    // put the walker into the array
-    walkerArray.push( tempWalker );
-}
 
   var videoNode = document.getElementsByTagName("video")[0];
   wrapper.appendChild(videoNode);
@@ -108,32 +95,24 @@ function setup() {
 
 function draw() {
 
-  // expose the pixels in the incoming video stream
+
   capture.loadPixels();
   mirrorVideo();
 
-  // if we have some pixels to work wtih them we should proceed
+
   if (capture.pixels.length > 0) {
 
-    // set up variables to test for the best pixel
+
     var bestLocations1 = [];
-    var bestLocations2 = [];
 
     for (var i = 0; i < capture.pixels.length; i += 8) {
-      // determine how close of a match this color is to our desired colors
       var match1 = dist(r1, g1, b1, capture.pixels[i], capture.pixels[i + 1], capture.pixels[i + 2]);
       if (match1 < threshold) {
-        // this pixel qualifies!  store its location into our array
         bestLocations1.push(i);
-      }
-      var match2 = dist(r2, g2, b2, capture.pixels[i], capture.pixels[i + 1], capture.pixels[i + 2]);
-      if (match2 < threshold) {
-        // this pixel qualifies!  store its location into our array
-        bestLocations2.push(i);
       }
     }
 
-    // draw the video
+    // draw the video only if we still need it!
     if (showCapture){
         image(capture, 0, 0);
     } else {
@@ -142,7 +121,6 @@ function draw() {
 
     // do we have a best match?  it's possible that no pixels met our threshold
     if (bestLocations1.length > 0) {
-      // average up all of our locations
       var xSum = 0;
       var ySum = 0;
       for (var i = 0; i < bestLocations1.length; i++) {
@@ -159,40 +137,15 @@ function draw() {
       rect(xPos1, yPos1, 25, 25);
     }
 
-    if (bestLocations2.length > 0) {
-      // average up all of our locations
-      var xSum = 0;
-      var ySum = 0;
-      for (var i = 0; i < bestLocations2.length; i++) {
-        xSum += (bestLocations2[i] / 4) % screenWidth;
-        ySum += (bestLocations2[i] / 4) / screenWidth;
-      }
-
-      // average our sums to get our 'centroid' point
-      var xPos2 = xSum / bestLocations2.length;
-      var yPos2 = ySum / bestLocations2.length;
-
-      // now we know the best match!  draw a box around it
-      stroke(255,0,0);
-      rect(xPos2, yPos2, 25, 25);
-    }
-
     pixelsPerFrame1 = dist(oldX1, oldY1, xPos1, yPos1);
     oldX1 = xPos1;
     oldY1 = yPos1;
     sumPPF1 += pixelsPerFrame1;
 
-    pixelsPerFrame2 = dist(oldX2, oldY2, xPos2, yPos2);
-    oldX2 = xPos2;
-    oldY2 = yPos2;
-    sumPPF2 += pixelsPerFrame2;
-
     if (frames % frameCheck == 0){
         averagePPF1 = sumPPF1 / 30;
-        averagePPF2 = sumPPF2 / 30;
         sumPPF1 = 0;
-        sumPPF2 = 0;
-        console.log(averagePPF1, averagePPF2);
+        console.log(averagePPF1);
     }
     frames++;
   }
@@ -210,16 +163,8 @@ function mousePressed() {
     console.log("Color 1 - Looking for: R=" + r1 + "; G=" + g1 + "; B=" + b1);
     currentColor = 2;
   }
-  else if (currentColor == 2) {
-    r2 = capture.pixels[loc];
-    g2 = capture.pixels[loc + 1];
-    b2 = capture.pixels[loc + 2];
 
-    console.log("Color 2 - Looking for: R=" + r2 + "; G=" + g2 + "; B=" + b2);
-    currentColor = 1;
-  }
-
-  if (r1 != 0 && g1 != 0 && b1 != 0 && r2 != 0 && g2 != 0 && b2 != 0){
+  if (r1 != 0 && g1 != 0 && b1 != 0){
       showCapture = false;
   }
 }
@@ -368,7 +313,7 @@ function NoiseWalker(x, y) {
     this.y += yMovement;
 
     // are we close to the mouse?  if so, run away!
-    if (dist(this.x, this.y, xPos1, yPos1) < 50) {
+    if (dist(this.x, this.y, xPos1, yPos1) < 25) {
       var speed = 1 + (averagePPF1/8);
       if (xPos1 < this.x) {
         this.x += speed;
